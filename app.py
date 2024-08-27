bff5e24627f7c7c5591e4402a25024227059a463
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-08-27 11:27:04 -0300

    defs

--- app.py
+++ app.py
@@ -1,15 +1,51 @@
-print("""
+import os
 
-██████████████████████████████████████████████████████████████████████████████████████████
-█▄─▄█▄─▀█▀─▄█─▄▄─█▄─▄─▀█▄─▄█▄─▄███▄─▄██▀▄─██▄─▄▄▀█▄─▄██▀▄─████▄─█─▄█▄─█▀▀▀█─▄█─▄▄▄▄█─▄▄▄▄█
-██─███─█▄█─██─██─██─▄─▀██─███─██▀██─███─▀─███─▄─▄██─███─▀─█████─▄▀███─█─█─█─██▄▄▄▄─█▄▄▄▄─█
-▀▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▀▄▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▀▄▄▀▄▄▀▀▀▄▄▀▄▄▀▀▄▄▄▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀
- """);
+def mostra_titulo():
+    print("""
 
-print("1. Cadastrar Clientes")
-print("2. Listar Clientes")
-print("3. Ativar Usuário")
-print("4. Sair")
+    ██████████████████████████████████████████████████████████████████████████████████████████
+    █▄─▄█▄─▀█▀─▄█─▄▄─█▄─▄─▀█▄─▄█▄─▄███▄─▄██▀▄─██▄─▄▄▀█▄─▄██▀▄─████▄─█─▄█▄─█▀▀▀█─▄█─▄▄▄▄█─▄▄▄▄█
+    ██─███─█▄█─██─██─██─▄─▀██─███─██▀██─███─▀─███─▄─▄██─███─▀─█████─▄▀███─█─█─█─██▄▄▄▄─█▄▄▄▄─█
+    ▀▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▀▄▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▀▄▄▀▄▄▀▀▀▄▄▀▄▄▀▀▄▄▄▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀
+    """);
 
-opcao_escolhida = input("Escolha um opção:")
-print("Você escolheu a opcção:", opcao_escolhida) 
+def mostra_escolhas():
+    print("1. Cadastrar proprietário")
+    print("2. Listar Imóveis")
+    print("3. Ativar Imóvel")
+    print("4. Sair")
+
+def escolhe_opcao():
+    
+    def finalizar_programa():
+        os.system("cls")
+        print("Flinalizando o progrma\n")
+    
+    def opcao_invalida():
+        print("Esse carácter não é permitido ")
+        input("Aperte qualquer tecla para voltar")
+        main()
+
+    try:
+        opcao_escolhida = int(input("Escolha um opção:"))
+        
+        if opcao_escolhida == 1:
+            print("Você escolher Cadastrar proprietário")
+        elif opcao_escolhida ==2:
+            print("Você escolher Listar Imóveis")
+        elif opcao_escolhida ==3:
+            print("Você escolher Ativar Imóvel")
+        elif opcao_escolhida ==4:
+            finalizar_programa()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+        mostra_titulo()
+        mostra_escolhas()
+        escolhe_opcao()
+
+if __name__=='__main__':
+        main()
